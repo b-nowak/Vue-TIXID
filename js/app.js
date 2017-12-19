8855514a72e85d675c4be51681710960be1cc81a
Bartosz Nowak <bartosz.nowak@polcode.net>
2017-12-19 11:17:52 +0100

    new player btn, refactor js

---
+++ js/app.js
@@ -0,0 +1,32 @@
+Vue.config.devtools = true;
+var app = new Vue({
+  el: "#app",
+  data: {
+    multiply: 3.34,
+    rows: []
+  },
+  methods: {
+    addRow: function () {
+      this.rows.push({
+        name: "Gracz",
+        width: 0,
+        score: 0
+      });
+    },
+    removeRow: function (index) {
+      var answer = confirm("Na sto pro?")
+      if (answer) {
+        this.rows.splice(index, 1);
+      }
+    },
+    increaseScore: function (row) {
+      row.width = row.width + this.multiply;
+      row.score++;
+    },
+    decreaseScore: function (row) {
+      row.width = row.width - this.multiply;
+      row.score--;
+    }
+
+  }
+});
